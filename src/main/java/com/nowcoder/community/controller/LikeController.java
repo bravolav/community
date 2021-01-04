314dfa7d29281a83a5a85668779e742f1f814804
theMocker <55192408@qq.com>
2021-01-04 23:08:47 +0800

    收到的赞记录 个人主页(4.4)

--- src/main/java/com/nowcoder/community/controller/LikeController.java
+++ src/main/java/com/nowcoder/community/controller/LikeController.java
@@ -24,11 +24,11 @@ public class LikeController {
 
     @RequestMapping(path = "/like", method = RequestMethod.POST)
     @ResponseBody
-    private String like(int entityType, int entityId) {
+    private String like(int entityType, int entityId, int entityUserId) {
         User user = hostHolder.getUser();
 
         // 点赞
-        likeService.like(user.getId(), entityType, entityId);
+        likeService.like(user.getId(), entityType, entityId, entityUserId);
 
         // 数量
         long likeCount = likeService.findEntityLikeCount(entityType, entityId);
